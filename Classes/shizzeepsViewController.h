9e1147035b5456255e47c636b1cfb8a5647916c2
Sue Brown <crunchy@houseofcrunchy.com>
2009-11-11 18:33:42 -0800

    Added JSON library

--- Classes/shizzeepsViewController.h
+++ Classes/shizzeepsViewController.h
@@ -14,6 +14,7 @@
 }
 
 - (void) loadShizzeeps;
+- (void) displayShizzeeps: (NSDictionary *)JSONDictionary;
 
 @end
 
--- Classes/shizzeepsViewController.m
+++ Classes/shizzeepsViewController.m
@@ -8,7 +8,7 @@
 
 #import "shizzeepsViewController.h"
 #import "passwords.h"
-
+#import "CJSONDeserializer.h"
 
 
 @implementation shizzeepsViewController
@@ -16,7 +16,7 @@
 
 - (void) loadShizzeeps {
 	
-	NSString *shizzowAPI = @"https://v0.api.shizzow.com/places?cities=Portland&states=OR&countries=US";
+	NSString *shizzowAPI = @"https://v0.api.shizzow.com/places?populated=true&cities=Portland&states=OR&countries=US";
 	
 	
 	NSURL *url = [[NSURL alloc] initWithString: shizzowAPI]; 
@@ -38,6 +38,8 @@
 
 - (void)connection:(NSURLConnection *)connection didReceiveAuthenticationChallenge:(NSURLAuthenticationChallenge *)challenge {
 		
+	// Note that username and password are in passwords.h, which is ignored by git.
+	
 	// from stack overflow
 	// http://stackoverflow.com/questions/1487874/securing-wcf-rest-service-for-use-with-iphone-application
 	NSURLCredential *newCredential; 
@@ -55,15 +57,38 @@
 
 - (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data {
 	NSLog (@"connectionDidReceiveData");
-	NSString *newText = [[NSString alloc]
-						 initWithData:data
-						 encoding:NSUTF8StringEncoding];
+	NSString *newText = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+	
 	if (newText != NULL) {
-		//[urlContentsView appendTextToView:newText];
-		NSLog(newText);
-		[newText release];
+		
+		NSString *jsonString = newText;
+		NSData *jsonData = [jsonString dataUsingEncoding:NSUTF32BigEndianStringEncoding];
+		NSError *error = nil;
+		NSDictionary *dictionary = [[CJSONDeserializer deserializer] deserializeAsDictionary:jsonData error:&error];
+	
+		[self displayShizzeeps:dictionary];
+		
+
 	}
 }
+
+
+// Iterate through the JSON and build a web page out of it, then display it in the webview
+- (void) displayShizzeeps:(NSDictionary *)JSONDictionary {
+	
+	
+	NSURL *baseURL = [[NSURL alloc] initWithString: @"http://shizzeps.com"];
+	
+	for (id key in JSONDictionary) {
+		NSLog(@"key: %@, value: %@", key, [JSONDictionary objectForKey:key]);
+		[webView loadHTMLString:key baseURL:baseURL];
+	}
+}
+
+
+
+
+
 	
 	
 /*
